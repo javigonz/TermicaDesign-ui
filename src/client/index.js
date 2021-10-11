0436f161c4b8ebc97793c23ec1a4a89174bfae5e
javier.gonzalez <javier.gonzalez@piksel.com>
2021-10-11 23:11:53 +0200

    fix

--- src/client/index.js
+++ src/client/index.js
@@ -10,7 +10,7 @@ const getWorks = async () => {
     url: API_RESOURCE_GET_WORKS,
     method: "GET",
     headers: {
-      "X-Api-Key": process.env.REACT_APP_API_KEY,
+      "X-Api-Key": "MwlT6zMnQ4aM2DKzGcyYMafADKBcid1o1MoYUXgj",
     },
   });
   return resp.data;
