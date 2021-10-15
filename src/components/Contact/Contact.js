4a7e0cdea1eb21b2eaf79b309d83aca73f2ca60d
javier.gonzalez <javier.gonzalez@piksel.com>
2021-10-15 20:51:41 +0200

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,12 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="main-content_contact">
-      <h1>Really soon ...</h1>
+      <iframe
+        width="600"
+        height="450"
+        loading="lazy"
+        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLSHbT8RZcg0RzzLKyZLcJWA&key=AIzaSyCcj1msu2deKS2RL8rm2CZCSukq-hVAp44"
+      ></iframe>
     </div>
   );
 }
