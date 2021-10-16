93427d6182c88776c6738f4e8ff485481b76d0bc
javier.gonzalez <javier.gonzalez@piksel.com>
2021-10-16 23:50:45 +0200

    add google maps api

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,39 @@
 import React from "react";
 import "./Contact.css";
+import axios from "axios";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 function Contact() {
+  const lib = ["places"];
+  const id = ["9efe2f9aff21c394"];
+  const key = "AIzaSyCcj1msu2deKS2RL8rm2CZCSukq-hVAp44";
+  const MalagaLocation = { lat: 36.7212, lng: -4.4217 };
+
   return (
     <div className="main-content_contact">
-      <iframe
-        width="600"
-        height="450"
-        loading="lazy"
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLSHbT8RZcg0RzzLKyZLcJWA&key=AIzaSyCcj1msu2deKS2RL8rm2CZCSukq-hVAp44"
-      ></iframe>
+      <div className="main-content_right">
+        <p>Test ...</p>
+      </div>
+      <div className="main-content_left"></div>
+      <div className="main-content_map ">
+        <LoadScript googleMapsApiKey={key} libraries={lib} mapIds={id}>
+          <GoogleMap
+            center={MalagaLocation}
+            zoom={7}
+            options={{ mapId: "9efe2f9aff21c394" }}
+            mapContainerStyle={{
+              height: "100%",
+              width: "100%",
+              position: "absolute",
+              top: "0px",
+              left: "0px",
+              backgroundColor: "#000",
+            }}
+          >
+            <Marker position={MalagaLocation} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
     </div>
   );
 }
